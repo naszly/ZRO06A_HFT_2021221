ca7d356250cbba42cef5b8a84036abff6be6a4bb
kornel <naszaly.k@gmail.com>
2021-11-16 22:40:16 +0100

    refactoring

--- ZRO06A_HFT_2021221.Repository/Repository.cs
+++ ZRO06A_HFT_2021221.Repository/Repository.cs
@@ -1,17 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZRO06A_HFT_2021221.Repository
 {
    public abstract class Repository<T> :
       IRepository<T> where T : class
    {
-      protected DbContext ctx;
+      protected readonly DbContext ctx;
 
-      public Repository(DbContext ctx)
+      protected Repository(DbContext ctx)
       {
          this.ctx = ctx;
       }
