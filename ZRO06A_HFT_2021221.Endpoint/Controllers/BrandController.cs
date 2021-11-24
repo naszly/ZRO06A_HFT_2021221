f679c2ba2e2f2a9032917f2577164291ff8996b9
kornel <naszaly.k@gmail.com>
2021-11-24 16:05:33 +0100

    controller

---
+++ ZRO06A_HFT_2021221.Endpoint/Controllers/BrandController.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ZRO06A_HFT_2021221.Logic;
+using ZRO06A_HFT_2021221.Models;
+
+namespace ZRO06A_HFT_2021221.Endpoint.Controllers
+{
+   [Route("[controller]")]
+   [ApiController]
+   public class BrandController : Controller
+   {
+      private readonly IBrandLogic brandLogic;
+
+      public BrandController(IBrandLogic brandLogic)
+      {
+         this.brandLogic = brandLogic;
+      }
+
+      [HttpGet]
+      public IEnumerable<Brand> Get()
+      {
+         return brandLogic.GetAll();
+      }
+
+      [HttpGet("{id}")]
+      public Brand Get(int id)
+      {
+         return brandLogic.GetOne(id);
+      }
+
+      [HttpPost]
+      public void Post([FromBody] Brand value)
+      {
+         brandLogic.Create(value);
+      }
+
+      [HttpPut]
+      public void Put([FromBody] Brand value)
+      {
+         brandLogic.Update(value);
+      }
+
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+         brandLogic.Delete(id);
+      }
+   }
+}
---
+++ ZRO06A_HFT_2021221.Endpoint/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ZRO06A_HFT_2021221.Logic;
+using ZRO06A_HFT_2021221.Models;
+
+namespace ZRO06A_HFT_2021221.Endpoint.Controllers
+{
+   [Route("[controller]")]
+   [ApiController]
+   public class CustomerController : Controller
+   {
+      private readonly ICustomerLogic customerLogic;
+
+      public CustomerController(ICustomerLogic customerLogic)
+      {
+         this.customerLogic = customerLogic;
+      }
+
+      [HttpGet]
+      public IEnumerable<Customer> Get()
+      {
+         return customerLogic.GetAll();
+      }
+
+      [HttpGet("{id}")]
+      public Customer Get(int id)
+      {
+         return customerLogic.GetOne(id);
+      }
+
+      [HttpPost]
+      public void Post([FromBody] Customer value)
+      {
+         customerLogic.Create(value);
+      }
+
+      [HttpPut]
+      public void Put([FromBody] Customer value)
+      {
+         customerLogic.Update(value);
+      }
+
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+         customerLogic.Delete(id);
+      }
+   }
+}
