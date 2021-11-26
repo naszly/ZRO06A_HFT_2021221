977d2c8027db26abdc126402aea731b1f98ba892
kornel <naszaly.k@gmail.com>
2021-11-26 16:22:57 +0100

    refactoring

--- ZRO06A_HFT_2021221.Models/Car.cs
+++ ZRO06A_HFT_2021221.Models/Car.cs
@@ -8,27 +8,13 @@ namespace ZRO06A_HFT_2021221.Models
    [Table("cars")]
    public class Car
    {
-      public Car() { }
-
-      public Car(string modelName, int id, int price)
+      public Car()
       {
-         Model = modelName;
-         BrandId = id;
-         BasePrice = price;
-      }
-
-      public Car(string modelName, int id)
-      {
-         Model = modelName;
-         BrandId = id;
+         Orders = new HashSet<Order>();
       }
 
       [Key]
       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-      // Part 1
-      [Column("car_id", TypeName = "int")]
-      // Part 2 auto key increment
-      //[Column("car_id", TypeName = "int", Order = 0)]
       public int Id { get; set; }
 
       [MaxLength(100)] [Required] public string Model { get; set; }
